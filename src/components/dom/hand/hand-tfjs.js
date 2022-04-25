b3c4466fcb29d713bb10a9e726fc940497fab694
Brandon Silva <brandons209@aol.com>
2022-04-25 16:18:39 -0400

    finalize gesture recognition

--- src/components/dom/hand/hand-tfjs.js
+++ src/components/dom/hand/hand-tfjs.js
@@ -39,6 +39,8 @@ let startInferenceTime,
 let inferenceTimeSum = 0,
   lastPanelUpdate = 0;
 let rafId;
+let current_gesture = "None";
+
 function createScatterGLContext(selectors) {
   const scatterGLEl = document.querySelector(selectors);
   return {
@@ -125,8 +127,57 @@ function endEstimateHandsStats() {
   //    }
 }
 
-export function normalizeRadians(angle) {
+function normalizeRadians(angle) {
   return angle - 2 * Math.PI * Math.floor((angle + Math.PI) / (2 * Math.PI));
+}
+
+function get_gesture(hand)
+{
+  const WRIST_JOINT = 0;
+  const MIDDLE_FINGER_PIP_JOINT = 6;
+  const INDEX_FINGER_PIP_JOINT = 4;
+  const RING_FINGER_PIP_JOINT = 8;
+  let gesture = "None";
+
+  const x0 = hand.keypoints3D[WRIST_JOINT].x * 640;
+  const y0 = hand.keypoints3D[WRIST_JOINT].y * 480;
+  const z0 = hand.keypoints3D[WRIST_JOINT].z * 480;
+
+  let x1 = (hand.keypoints3D[INDEX_FINGER_PIP_JOINT].x + hand.keypoints3D[RING_FINGER_PIP_JOINT].x) / 2;
+  let y1 = (hand.keypoints3D[INDEX_FINGER_PIP_JOINT].y + hand.keypoints3D[RING_FINGER_PIP_JOINT].y) / 2;
+  let z1 = (hand.keypoints3D[INDEX_FINGER_PIP_JOINT].z + hand.keypoints3D[RING_FINGER_PIP_JOINT].z) / 2;
+
+  x1 = (x1 + hand.keypoints3D[MIDDLE_FINGER_PIP_JOINT].x) / 2 * 640;
+  y1 = (y1 + hand.keypoints3D[MIDDLE_FINGER_PIP_JOINT].y) / 2 * 480;
+  z1 = (z1 + hand.keypoints3D[MIDDLE_FINGER_PIP_JOINT].z) / 2 * 480;
+
+  const rotation_y = normalizeRadians(Math.PI / 2 - Math.atan2(-(y1 - y0), x1 - x0)) * 180 / Math.PI;
+  const rotation_z = normalizeRadians(Math.PI / 2 - Math.atan2(-(z1 - z0), x1 - x0)) * 180 / Math.PI;
+
+  let index_top = new THREE.Vector3(hand.keypoints3D[7].x, hand.keypoints3D[7].y, hand.keypoints3D[7].z);
+  let middle_top = new THREE.Vector3(hand.keypoints3D[11].x, hand.keypoints3D[11].y, hand.keypoints3D[11].z);
+  let thumb_top = new THREE.Vector3(hand.keypoints3D[4].x, hand.keypoints3D[4].y, hand.keypoints3D[4].z);
+
+  const index_dist = index_top.distanceTo(thumb_top);
+  const middle_dist = middle_top.distanceTo(thumb_top);
+
+  // scapel gesture
+  if (index_dist <= 0.04 && middle_dist <= 0.04 && rotation_z >=  -180 && rotation_z <= 0 && rotation_y >= -50 && rotation_y <= 100)
+  {
+    gesture = "Scapel";
+  }
+  else
+  {
+    gesture = "None";
+  }
+  console.log(`Thumb to middle: ${middle_dist}\nThumb to index: ${index_dist}\nRot XY: ${rotation_y}\nRot XZ: ${rotation_z}`);
+
+  return gesture;
+}
+
+function update_gesture(hand)
+{
+  current_gesture = get_gesture(hand);
 }
 
 async function renderResult() {
@@ -165,10 +216,12 @@ async function renderResult() {
   // print orientation using quaternions for first hand detected
   if (hands && hands.length > 0)
   {
+    const hand = hands[0];
+    update_gesture(hand);
+    console.log(current_gesture);
     // calculate orientation of hand to camera
     // TODO: add in gestures for holding scapel, check distances between fingers and hands, and use quaternion for proper hand orientation
-    const hand = hands[0];
-    const camera_unit = new THREE.Vector3(0, 1, 0);
+    /*const camera_unit = new THREE.Vector3(0, 1, 0);
     let wrist = hand.keypoints3D[0];
     let thumb = hand.keypoints3D[1];
     let middle = hand.keypoints3D[10]
@@ -188,7 +241,7 @@ async function renderResult() {
 
     const quaternion = new THREE.Quaternion();
     quaternion.setFromAxisAngle(wrist, thumb.angleTo(camera_unit));
-    // console.log(quaternion);
+    // console.log(quaternion);*/
 
     // detect scapel gesture
     /*
@@ -213,7 +266,7 @@ async function renderResult() {
     middle_top.sub(middle_mid);
     middle_bot.sub(middle_mid);
     // apply quaternion to these vectors so they match the orientation of the hand before calculating angle
-    middle_top.applyQuaternion(quaternion);
+    middle_top.applyQuaternion(quaternion);console.log("Scapel");
     middle_bot.applyQuaternion(quaternion);
     const middle_angle = middle_top.angleTo(middle_bot);
 
@@ -237,28 +290,8 @@ async function renderResult() {
     {
       console.log(`Thumb to middle: ${thumb.distanceTo(middle)}\nThumb to index: ${thumb.distanceTo(index)}\nW: ${quaternion.w}`);
     }*/
-    const WRIST_JOINT = 0;
-    const MIDDLE_FINGER_PIP_JOINT = 6;
-    const INDEX_FINGER_PIP_JOINT = 4;
-    const RING_FINGER_PIP_JOINT = 8;
-    const x0 = hand.keypoints3D[WRIST_JOINT].x * 640;
-    const y0 = hand.keypoints3D[WRIST_JOINT].y * 480;
-
-    let x1 = (hand.keypoints3D[INDEX_FINGER_PIP_JOINT].x +
-              hand.keypoints3D[RING_FINGER_PIP_JOINT].x) /
-        2;
-    let y1 = (hand.keypoints3D[INDEX_FINGER_PIP_JOINT].y +
-              hand.keypoints3D[RING_FINGER_PIP_JOINT].y) /
-        2;
-    x1 = (x1 + hand.keypoints3D[MIDDLE_FINGER_PIP_JOINT].x) / 2 * 640;
-    y1 = (y1 + hand.keypoints3D[MIDDLE_FINGER_PIP_JOINT].y) / 2 * 480;
-
-    const rotation = normalizeRadians(Math.PI / 2 - Math.atan2(-(y1 - y0), x1 - x0));
-
-    console.log((rotation * 180) / Math.PI);
 
   }
-
 
   camera.drawCtx();
 
